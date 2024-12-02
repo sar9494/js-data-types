4710948d3a8d691526803f1bfdf55137b35c193b
sar9494 <batbayarsaruul0@gmail.com>
2024-12-02 16:46:22 +0800

    jhfs

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-ifhoaho
+console.log("hello world js");
+//string =>"text"
+//number => 3 39 9
+//Boolean => true / false
+// Object=> { key:value }
+//Array => [ index, index ]
+// Null Undefine => no value
